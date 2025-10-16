13601c930d0486ac9e369ede95de1ea835112e3b
NikolayMSiloworks <nikolay.milanov@siloworks.net>
2025-10-16 10:44:37 +0300

    refactor: changed how on pointer down is handled on door

--- src/prefabs/Door.ts
+++ src/prefabs/Door.ts
@@ -34,7 +34,7 @@ export default class Door extends Container {
         this.closedSprite = this.loadSprite(this.config.closed, 1);
         this.closedSprite.eventMode = 'static';
         this.closedSprite.cursor = "pointer";
-        this.closedSprite.on('pointerdown', this.handleClick.bind(this));
+        this.closedSprite.on('pointerdown', (e) => this.handleClick(e));
         this.handleShadowSprite = this.loadSprite(this.config.handleShadow, 1);
         this.handleSprite = this.loadSprite(this.config.handle, 1);
 
